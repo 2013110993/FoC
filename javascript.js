080da965f399552ecd78155aa86db80f46ff7fb1
2013110993 <2013110993@ub.edu.bz>
2020-11-02 15:20:50 -0600

    Added Video to Concept

--- javascript.js
+++ javascript.js
@@ -55,11 +55,11 @@ function and(){
 	
     
     if(inputOneIsOn&&inputTwoIsOn)
-        document.getElementById('andGate').src='images\\on.png';
+        document.getElementById('andGate').src='images\\and2on.png';
     else if (!inputOneIsOn && inputTwoIsOn) 
-        document.getElementById('andGate').src='..\\images\\off.png';
+        document.getElementById('andGate').src='images\\andoffon.png';
 	else if (inputOneIsOn && !inputTwoIsOn) 
-        document.getElementById('andGate').src='images\\on.png';
+        document.getElementById('andGate').src='images\\andonoff.png';
 	else if (!inputOneIsOn && !inputTwoIsOn) 
-        document.getElementById('andGate').src='images\\off.png';
+        document.getElementById('andGate').src='images\\and2off.png';
 	}
